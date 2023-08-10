b9a516208ec3b00336810b498ad2d33f0ec6f3ab
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2023-08-10 06:49:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_eip" "hashicat" {
   instance = aws_instance.hashicat.id
-  vpc      = true
+  domain      = hashicat
 }
 
 resource "aws_eip_association" "hashicat" {
